96fa925f318b19f35a6c097fb5553608f6aadba4
johanrussouw <jrussouw610505@gmail.com>
2026-03-01 15:05:39 +0200

    added resend and sending emails tested

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,9 @@
 import { inngest } from "@/app/utlis/inngest/client";
 import { serve } from "inngest/next";
-import { handleJobExpiration } from "./functions";
+import { handleJobExpiration, sendPreriodicJobListing } from "./functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [handleJobExpiration],
+  functions: [handleJobExpiration, sendPreriodicJobListing],
 });
