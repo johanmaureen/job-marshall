d1083fb3b69103edb79194fe7c267cb0e14bce7b
johanrussouw <jrussouw610505@gmail.com>
2026-03-08 12:39:15 +0200

    finished with skeleton for joblisting

--- app/(mainlayout)/page.tsx
+++ app/(mainlayout)/page.tsx
@@ -1,12 +1,16 @@
 import { JobFilters } from "@/components/general/JobFilters";
 import { JobListing } from "@/components/general/JobListings";
+import { JobListingLoading } from "@/components/general/JobListingsLoading";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-3 gap-8">
       <JobFilters />
       <div className="col-span-2 flex flex-col gap-6">
-        <JobListing />
+        <Suspense fallback={<JobListingLoading />}>
+          <JobListing />
+        </Suspense>
       </div>
     </div>
   );
