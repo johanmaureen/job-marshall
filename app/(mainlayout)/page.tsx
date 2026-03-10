3d4070179347f25eab237256b2a4ab55eb98f420
johanrussouw <jrussouw610505@gmail.com>
2026-03-10 10:00:07 +0200

    finished with pagination

--- app/(mainlayout)/page.tsx
+++ app/(mainlayout)/page.tsx
@@ -3,13 +3,21 @@ import { JobListing } from "@/components/general/JobListings";
 import { JobListingLoading } from "@/components/general/JobListingsLoading";
 import { Suspense } from "react";
 
-export default function Home() {
+type SearchParams = {
+  searchParams: Promise<{
+    page?: string;
+  }>;
+};
+
+export default async function Home({ searchParams }: SearchParams) {
+  const params = await searchParams;
+  const currentPage = Number(params.page) || 1;
   return (
     <div className="grid grid-cols-3 gap-8">
       <JobFilters />
       <div className="col-span-2 flex flex-col gap-6">
-        <Suspense fallback={<JobListingLoading />}>
-          <JobListing />
+        <Suspense fallback={<JobListingLoading />} key={currentPage}>
+          <JobListing currentPage={currentPage} />
         </Suspense>
       </div>
     </div>
--- components/general/JobListings.tsx
+++ components/general/JobListings.tsx
@@ -3,42 +3,55 @@ import { EmptyState } from "./EmtyState";
 import { JobCard } from "./JobCard";
 import { MainPagination } from "./MainPagination";
 
-async function getData() {
-  const data = await prisma.jobPost.findMany({
-    where: {
-      status: "ACTIVE",
-    },
-    select: {
-      jobTitle: true,
-      id: true,
-      salaryFrom: true,
-      salaryTo: true,
-      employmentType: true,
-      location: true,
-      createdAt: true,
-      company: {
-        select: {
-          name: true,
-          logo: true,
-          location: true,
-          about: true,
+async function getData(page: number = 1, pageSize: number = 2) {
+  const skip = (page - 1) * pageSize;
+  const [data, totalCount] = await Promise.all([
+    prisma.jobPost.findMany({
+      where: {
+        status: "ACTIVE",
+      },
+      take: pageSize,
+      skip: skip,
+      select: {
+        jobTitle: true,
+        id: true,
+        salaryFrom: true,
+        salaryTo: true,
+        employmentType: true,
+        location: true,
+        createdAt: true,
+        company: {
+          select: {
+            name: true,
+            logo: true,
+            location: true,
+            about: true,
+          },
         },
       },
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-  return data;
+      orderBy: {
+        createdAt: "desc",
+      },
+    }),
+    prisma.jobPost.count({
+      where: {
+        status: "ACTIVE",
+      },
+    }),
+  ]);
+  return {
+    jobs: data,
+    totalPages: Math.ceil(totalCount / pageSize),
+  };
 }
 
-export async function JobListing() {
-  const data = await getData();
+export async function JobListing({ currentPage }: { currentPage: number }) {
+  const { jobs, totalPages } = await getData(currentPage);
   return (
     <>
-      {data.length > 0 ? (
+      {jobs.length > 0 ? (
         <div className="flex flex-col gap-6">
-          {data.map((job) => (
+          {jobs.map((job) => (
             <JobCard key={job.id} job={job} />
           ))}
         </div>
@@ -51,7 +64,7 @@ export async function JobListing() {
         />
       )}
       <div className="flex justify-center mt-6">
-        <MainPagination totalPages={20} currentPage={15} />
+        <MainPagination totalPages={totalPages} currentPage={currentPage} />
       </div>
     </>
   );
